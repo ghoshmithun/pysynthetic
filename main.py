bcf8f706bac3a4531de7c40025bb4d241db22f0c
Mithun Ghosh <email.mithun.ghosh@gmail.com>
2021-05-03 21:32:54 +0530

    sample notebooks

--- main.py
+++ main.py
@@ -3,7 +3,12 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import os
-os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices'
+os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_s'
+os.environ['XLA_FLAGS'] = '--tf_xla_enable_xla_s'
+import tensorflow as tf
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+tf.config.experimental.list_physical_devices('GPU')
+
 
 # The above is for using GPU in tensorflow code
 
--- setup.py
+++ setup.py
@@ -10,17 +10,17 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 
 setup(name='synthetic_data',
-      description='Synthetic data generation methods with different synthetization methods.',
+      description='Synthetic data generation methods with different synthesization methods.',
       long_description=long_description,
       long_description_content_type='text/markdown',
       author='Mithun Ghosh',
       author_email='2018id04605@wilp.bits-pilani.ac.in',
-      keywords='data science ydata',
+      keywords='data science pysynthetic',
       url='',
       license="",
       python_requires=">=3.6, <3.9",
-      packages=find_namespace_packages('synthetic_module'),
-      package_dir={'':'synthetic_module'},
+      packages=find_namespace_packages('pysynthetic'),
+      package_dir={'':'pysynthetic'},
       include_package_data=True,
       options={"bdist_wheel": {"universal": True}},
       install_requires=requirements)
